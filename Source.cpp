467fe696d03509771b7592a57ff8597b4bb64dd4
nicolasrcp123 <nicolas19992016@hotmail.com>
2017-06-20 10:00:04 -0500

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,69 +1,113 @@
 #include <iostream>
+#include <conio.h>
+#include <vector>
+#include <deque>
+#include <list>
+#include <stack>
 using namespace std;
-//creamos la clase base
-class Base
-{
-	//funciones que imprimen en pantalla algunos mensajes
-	void a()
-	{
-		cout << "aprendi  ";
-	}
-	void c()
-	{
-		cout << "c++  ";
-	}
-	void e()
-	{
-		cout << "My hero  ";
-	}
 
-	virtual void ph1() = 0;
-	virtual void ph2() = 0;
 
+template <class T> class Queue;
+
+template <class T>
+
+Queue<T> operator +(const Queue<T> &s1, const Queue<T> &s2) {
+	Queue <T> result = s1;
+	for (unsigned i = 0;i<s1.items.size();++i) {
+		result.items.push_back(s2.items[i]);
+	}
+	return result;
+}
+template <class T>
+ostream &operator <<(ostream &o, Queue<T> &s1) {
+	
+	o << "   /\  " << endl;
+	o << "  /  \  " << endl;
+	o << " /    \ " << endl;
+	o << "/      \ "<< endl;
+	o << "--------" << endl;
+	for (unsigned i = 0;i<s1.items.size();++i) {
+
+		o << "  " << "|" << s1.items[i] << "|" << endl;
+		o << "  " << "---" << endl;
+
+	}
+	o << "**    **" << endl;
+	o << "**    **" << endl;
+	o << "^^    ^^" << endl;
+
+	return o;
+
+}
+template <class T>
+class Queue {
+	friend ostream &operator <<<>(ostream &o, Queue<T> &s1);
+	friend Queue<T> operator +<>(const Queue<T> &s1, const Queue<T> &s2);
+	friend Queue<T> operator -<>(const Queue<T> &s1, const Queue<T> &s2);
+	vector<T> items;
 public:
-	//Funcion que llama a otras funciones privadas para imprimir los valores de estas funciones
-	void execute()
-	{
-		a();
-		ph1();
-		c();
-		ph2();
-		e();
-	}
-};
+	bool empty() const { return items.empty(); }
+	void push(const T &item) { items.push_back(item); }
 
-class One : public Base {
-	//Funciones que heredan de la clase base(virtual puro) 
-	void ph1() {
-		cout << "bastante  ";
+	T pop() {
+		T last = items.back();
+
+		items.pop_back();
+		return last;
 	}
 
-	void ph2()
-	{
-		cout << "Gracias  ";
-	}
+}
+;
 
-};
+template <class T>
 
-class Two : public Base {
-	//Funciones que heredan de la clase base(virtual puro) 
-	void ph1() {
-		cout << "pero mucho molestabas cuando te preguntaba de ";
-	}
+Queue<T> operator -(const Queue<T> &s1, const Queue<T> &s2) {
+	Queue<T> result;
+	vector<int> c1;
+	for (unsigned i = 0;i < s1.items.size();i++) {
+		bool repite = true;
+		for (unsigned j = 0;j < s2.items.size();j++) {
+			if (s1.items[i] == s2.items[j]) {
+				c1.push_back(s1.items[i]);
+				repite = false;
+				break;
 
-	void ph2() {
-		cout << "MrMusculos ";
+			}
+		
+		}
+		if (repite == true) {
+			result.push(s1.items[i]);
+		}
+		
 	}
-};
+	for (unsigned i = 0;i < s2.items.size();i++) {
+		for(unsigned j=0;j<c1.size();j++){
+			if (s2.items[i] != c1[j]) {
+				result.items.push_back(s2.items[i]);
+			}
+		}
+	}
+	return result;
+}
+
+
 
 int main() {
-	//creamos un arreglo de punteros que contiene a la clase one y two  
-	Base *array[] = { &One(), &Two() };
-	//hacemos un bucle para llamar sus funciones de cada elemento de mi arreglo
-	for (int i = 0; i < 2; i++) {
-		array[i]->execute();
-		cout << '\n';
-	}
-	system("pause");
+	Queue<int> a, b;
+	a.push(3);
+	a.push(1);
+	a.push(4);
 
+	b.push(6);
+	b.push(1);
+	b.push(2);
+	Queue<int> c = a - b;
+	cout<<c << endl;;
+
+
+
+
+
+	system("pause");
+	return 0;
 }
